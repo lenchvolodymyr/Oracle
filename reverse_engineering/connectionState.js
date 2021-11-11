501bfe21aedf542c461610fb96d91af152724bd2
Lench Volodymyr <volodymyr.lench@binary-studio.com>
2021-11-11 15:51:42 +0200

    polish reverse engineering folder and allow adding only json types in json data type

--- reverse_engineering/connectionState.js
+++
@@ -1,8 +0,0 @@
-const { getConnectionClient } = require('./databaseService/databaseService');
-
-module.exports = {
-	_client: null,
-	getClient: () => this._client,
-	setClient: async connectionInfo => this._client = await getConnectionClient(connectionInfo),
-	clearClient: () => this._client = null,
-}
