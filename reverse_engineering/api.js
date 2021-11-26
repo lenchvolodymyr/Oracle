6aa71a39a1fc6d5846e9473d46476322842cf025
BeardimonR <beardimon2001@gmail.com>
2021-11-26 17:40:32 +0200

    impl basic auth and ddl retrieval

--- reverse_engineering/api.js
+++ reverse_engineering/api.js
@@ -1,20 +1,38 @@
 'use strict';
 
+const oracleHelper = require('./helpers/oracleHelper');
 const logInfo = require('./helpers/logInfo');
+const { setDependencies, dependencies } = require('./helpers/appDependencies');
+let _;
 
 module.exports = {
 	async connect(connectionInfo, logger, callback, app) {
-		return {};
+		initDependencies(app);
+		logger.clear();
+		logger.log('info', connectionInfo, 'connectionInfo', connectionInfo.hiddenKeys);
+		try {
+			await oracleHelper.connect(logger, connectionInfo);
+			callback();
+		} catch (err) {
+			handleError(logger, err, callback);
+			throw err;
+		}
 	},
 
-	disconnect(connectionInfo, logger, callback, app) {
-		callback();
+	async disconnect(connectionInfo, logger, callback) {
+		logInfo('Disconnecting from database', connectionInfo, logger);
+		try {
+			await oracleHelper.disconnect();
+			callback(null);
+		} catch (err) {
+			handleError(logger, err, callback);
+		}
 	},
 
 	async testConnection(connectionInfo, logger, callback, app) {
 		try {
 			logInfo('Test connection', connectionInfo, logger);
-			await this.connect(connectionInfo);
+			await this.connect(connectionInfo, logger, () => {}, app);
 			callback(null);
 		} catch(error) {
 			logger.log('error', { message: error.message, stack: error.stack, error }, 'Test connection');
@@ -23,18 +41,20 @@ module.exports = {
 	},
 
 	getDatabases(connectionInfo, logger, callback, app) {
+		debugger;
 		callback();
 	},
 
 	getDocumentKinds(connectionInfo, logger, callback, app) {
+		debugger;
 		callback();
 	},
 
 	async getDbCollectionsNames(connectionInfo, logger, callback, app) {
 		try {
 			logInfo('Retrieving databases and tables information', connectionInfo, logger);
-			const client = await this.connect(connectionInfo);
-			
+			await this.connect(connectionInfo, logger, () => {}, app);
+			const objects = await oracleHelper.getEntitiesNames();
 			callback(null, objects);
 		} catch(error) {
 			logger.log('error', { message: error.message, stack: error.stack, error }, 'Retrieving databases and tables information');
@@ -43,14 +63,123 @@ module.exports = {
 	},
 
 	async getDbCollectionsData(collectionsInfo, logger, callback, app) {
+		debugger;
 		try {
 			logger.log('info', collectionsInfo, 'Retrieving schema', collectionsInfo.hiddenKeys);
+			initDependencies(app);
 			logger.progress({ message: 'Start reverse-engineering process', containerName: '', entityName: '' });
-			
-			callback(null, [], null, []);
+			const data = collectionsInfo.collectionData;
+			const collections = data.collections;
+			const dataBaseNames = data.dataBaseNames;
+			const entitiesPromises = await dataBaseNames.reduce(async (packagesPromise, schema) => {
+				debugger;
+				const packages = await packagesPromise;
+				const entities = oracleHelper.splitEntityNames(collections[schema]);
+
+				const containerData = await oracleHelper.getContainerData(schema);
+
+				const tablesPackages = entities.tables.map(async table => {
+					const fullTableName = oracleHelper.getFullEntityName(schema, table);
+					logger.progress({ message: `Start getting data from table`, containerName: schema, entityName: table });
+					const ddl = await oracleHelper.getDDL(table);
+					const quantity = await oracleHelper.getRowsCount(fullTableName);
+
+					logger.progress({ message: `Fetching record for JSON schema inference`, containerName: schema, entityName: table });
+
+					const { documents, jsonSchema } = await oracleHelper.getJsonSchema(logger, getCount(quantity, data.recordSamplingSettings), fullTableName);
+					const entityData = await oracleHelper.getEntityData(fullTableName);
+
+					logger.progress({ message: `Schema inference`, containerName: schema, entityName: table });
+
+					const handledDocuments = oracleHelper.handleComplexTypesDocuments(jsonSchema, documents);
+
+					logger.progress({ message: `Data retrieved successfully`, containerName: schema, entityName: table });
+
+					return {
+						dbName: schema,
+						collectionName: table,
+						entityLevel: entityData,
+						documents: handledDocuments,
+						views: [],
+						ddl: {
+							script: ddl,
+							type: 'snowflake'
+						},
+						emptyBucket: false,
+						validation: {
+							jsonSchema
+						},
+						bucketInfo: {
+							indexes: [],
+							database: schema,
+							...containerData
+						}
+					};
+				});
+
+				const views = await Promise.all(entities.views.map(async view => {
+					const fullViewName = oracleHelper.getFullEntityName(schema, view);
+					logger.progress({ message: `Start getting data from view`, containerName: schema, entityName: view });
+					const ddl = await oracleHelper.getViewDDL(fullViewName);
+					const viewData = await oracleHelper.getViewData(fullViewName);
+
+					logger.progress({ message: `Data retrieved successfully`, containerName: schema, entityName: view });
+
+					return {
+						name: view,
+						data: viewData,
+						ddl: {
+							script: ddl,
+							type: 'snowflake'
+						}
+					};
+				}));
+
+				if (_.isEmpty(views)) {
+					return [ ...packages, ...tablesPackages ];
+				}
+
+				const viewPackage = Promise.resolve({
+					dbName: schemaName,
+					entityLevel: {},
+					views,
+					emptyBucket: false,
+					bucketInfo: {
+						indexes: [],
+						database,
+						...containerData
+					}
+				});
+
+				return [ ...packages, ...tablesPackages, viewPackage ];
+			}, Promise.resolve([]));
+
+			const packages = await Promise.all(entitiesPromises).catch(err => callback(err));
+			callback(null, packages.filter(Boolean));
 		} catch (error) {
 			logger.log('error', { message: error.message, stack: error.stack, error }, 'Reverse-engineering process failed');
 			callback({ message: error.message, stack: error.stack })
 		}
 	}
+};
+
+const handleError = (logger, error, cb) => {
+	const message = _.isString(error) ? error : _.get(error, 'message', 'Reverse Engineering error')
+	logger.log('error', { error }, 'Reverse Engineering error');
+
+	cb(message);
+};
+
+const getCount = (count, recordSamplingSettings) => {
+	const per = recordSamplingSettings.relative.value;
+	const size = (recordSamplingSettings.active === 'absolute')
+		? recordSamplingSettings.absolute.value
+		: Math.round(count / 100 * per);
+	return size;
+};
+
+const initDependencies = app => {
+	setDependencies(app);
+	_ = dependencies.lodash;
+	oracleHelper.setDependencies(dependencies);
 };
